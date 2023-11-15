a9392d6122db5d6712929c1c67c27ee64bcaa76d
corners2wall <akosto4kin@gmail.com>
2023-11-15 22:41:24 +0300

    fix: change styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
 import { ErrorBoundary } from 'react-error-boundary';
 import fallbackRender from './components/FallbackRender';
-import Home from './pages/Home';
-import Custom from './pages/Custom';
-import Example from './pages/Example';
 import NotFound from './pages/NotFound';
 
 export default function Application() {
--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,11 +1,16 @@
 import { ReactComponent as Cosmonaut } from '../../assets/icons/cosmonaut.svg';
+import getImageUrl from '../../utils/getImageUrl';
+//ToDo pass svg as image for out of bundle
+const filmGrain = getImageUrl('peopleAnimation', 'gif');
 
 export default function PageNotFound() {
   return (
-    <div className="h-screen  bg-black bg-[url('white_grain.png'),_url('film_grain.png')] text-white py-16">
-      <div className='flex w-[1170px] mx-auto h-full items-center'>
+    <div
+      className={`h-screen bg-black bg-[url('/white_grain.png'),_url('/film_grain.png')] text-white py-16`}
+    >
+      <div className='flex w-fit mx-auto h-full items-center flex-wrap-reverse'>
         <div className='flex flex-col w-1/2'>
-          <h3 className='text-[50px] whitespace-nowrap tracking-wide'>
+          <h3 className='text-[55px] whitespace-nowrap tracking-wide'>
             Oops! You ran out of oxygen.
           </h3>
           <p className='text-[20px] font-light mt-8 tracking-wide'>
@@ -18,8 +23,11 @@ export default function PageNotFound() {
           <span className='relative text-[360px] mb-96 font-bold'>
             404
             <Cosmonaut className='animate-resizeToHidden top-0 bottom-0 left-0 right-0 mx-auto w-fit h-fit absolute' />
+            {/* <img src={filmGrain} className='w-1/2' /> */}
           </span>
         </div>
+        {/* for fire class */}
+        <div className='animate-hidden-visible' />
       </div>
     </div>
   );
