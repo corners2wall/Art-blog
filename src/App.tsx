4e754bf12a868270ab2fd61d9dd44faf289887d1
corners2wall <akosto4kin@gmail.com>
2023-12-26 10:55:17 +0300

    feat: base image scale

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import { ErrorBoundary } from 'react-error-boundary';
 import fallbackRender from './components/FallbackRender';
 import NotFound from './pages/NotFound';
+import Custom from './pages/Custom';
+import Home from './pages/Home';
+import Example from './pages/Example';
 
 export default function Application() {
   return (
     <ErrorBoundary fallbackRender={fallbackRender}>
       {/* <Home /> */}
-      {/* <Custom /> */}
+      <Custom />
       {/* <Example /> */}
-      <NotFound />
+      {/* <NotFound /> */}
     </ErrorBoundary>
   );
 }
--- src/pages/Custom/Custom.tsx
+++ src/pages/Custom/Custom.tsx
@@ -1,58 +1,115 @@
-import { useState } from 'react';
-import Button from '../../components/Button';
-import useMousePosition from '../../hooks/useMousePosition';
+import getImageUrl from '../../utils/getImageUrl';
+import { PropsWithChildren, forwardRef, useEffect, useRef } from 'react';
+import { motion, useAnimate } from 'framer-motion';
 
+const background = getImageUrl('background', 'jpg');
+const getScaleValue = (value: number, scale: number) => value * (1 / (1 - scale));
+
+interface ImageBlockProps {
+  imageSrc: string;
+  className?: string;
+}
+
+const ImageBlock = forwardRef<HTMLDivElement, ImageBlockProps>(({ imageSrc, className }, ref) => {
+  return (
+    <div className={`w-screen h-screen max-h-screen relative ${className}`} ref={ref}>
+      <img className='w-full h-full object-cover' src={imageSrc} />
+    </div>
+  );
+});
+
+function ImageRow({ children }: PropsWithChildren) {
+  return <div className='flex gap-5'>{children}</div>;
+}
+
+function TileWrapper({ children }: PropsWithChildren) {
+  return (
+    <div className='absolute -left-1/2 -top-2/4 flex flex-col items-center gap-5'>{children}</div>
+  );
+}
+
+// ref: https://www.somefolk.co.uk/
 export default function Custom() {
-  // ref: https://www.somefolk.co.uk/
-  const mousePosition = useMousePosition();
-  const [scale, setScale] = useState<any>({});
+  const anchorRef = useRef<HTMLDivElement>(null);
+
+  const [scope, animate] = useAnimate<HTMLDivElement>();
+
+  useEffect(() => {
+    if (anchorRef.current) {
+      const { x, width, y } = anchorRef.current.getBoundingClientRect();
+
+      const leftOffset = getScaleValue(x, 0.5);
+      const topOffset = getScaleValue(y, 0.5);
+
+      length = x - width / 2;
+
+      animate(
+        scope.current,
+        {
+          transform: `translate(${-length}px) scale(0.5)`,
+          opacity: [0, 1],
+          filter: ['brightness(60%)', 'brightness(85%)'],
+        },
+        { duration: 2 }
+      ).then(() => {
+        anchorRef.current!.animate(
+          { boxShadow: 'inset 0px 0px 113px 43px rgba(0,0,0,0.53)' },
+          {
+            duration: 1000,
+            fill: 'forwards',
+            pseudoElement: '::after',
+            easing: 'cubic-bezier(.06,.66,.04,.88)',
+          }
+        );
+        animate(
+          scope.current,
+          {
+            transform: `translate(-${leftOffset}px, -${topOffset}px) scale(1)`,
+            filter: 'brightness(50%)',
+          },
+          { duration: 1.5 }
+        );
+      });
+    }
+  }, []);
+
+  const pseudoShadow = `after:content-[''] after:absolute after:top-0 after:left-0 after:right-0 after:bottom-0`;
 
   return (
-    <div className='flex'>
-      <Button
-        className='fixed top-0 left-0 z-10'
-        label='scale'
-        onClick={() => {
-          // scale(2,2)
-          setScale({
-            transform: `translateX(-1200px) scale(.5)`, // translateY(100px) translateZ(0px)`,
-            // transform: `translateX(${mousePosition.x}px) translateY(${mousePosition.y}px) translateZ(0px)`,
-            // transform: 'scale(.5)',
-            // transformOrigin: `${mousePosition.x}px ${mousePosition.y}px`,
-          });
-          console.log(mousePosition);
-        }}
-      />
-      <div
-        style={{ ...scale }}
-        className='flex flex-col gap-5 scale-[0.5] items-center absolute 
-        duration-[2000ms]'
-      >
-        {/* top-[-100%] left-[-175%]  */}
-        <div className='flex gap-5'>
-          <div className='w-screen min-h-screen bg-slate-400 relative'>1</div>
-          <div className='w-screen min-h-screen bg-slate-400 relative'>2</div>
-          <div className='w-screen min-h-screen bg-slate-400 relative'>3</div>
-          <div className='w-screen min-h-screen bg-slate-400 relative'>4</div>
-          <div className='w-screen min-h-screen bg-slate-400 relative'>5</div>
-          <div className='w-screen min-h-screen bg-slate-400 relative'>6</div>
-        </div>
-        <div className='flex gap-5'>
-          <div className='w-screen min-h-screen bg-slate-400 relative'>1</div>
-          <div className='w-screen min-h-screen bg-slate-400 relative'>2</div>
-          <div className='w-screen min-h-screen bg-slate-400 relative'>3</div>
-          <div className='w-screen min-h-screen bg-slate-400 relative'>4</div>
-          <div className='w-screen min-h-screen bg-slate-400 relative'>5</div>
-        </div>
-        <div className='flex gap-5'>
-          <div className='w-screen min-h-screen bg-slate-400 relative'>1</div>
-          <div className='w-screen min-h-screen bg-slate-400 relative'>2</div>
-          <div className='w-screen min-h-screen bg-slate-400 relative'>3</div>
-          <div className='w-screen min-h-screen bg-slate-400 relative'>4</div>
-          <div className='w-screen min-h-screen bg-slate-400 relative'>5</div>
-          <div className='w-screen min-h-screen bg-slate-400 relative'>6</div>
-        </div>
-      </div>
+    <div className='overflow-hidden bg-[#333333]'>
+      <motion.div className='scale-50 min-w-[100vw] min-h-[100vh] origin-top-left	' ref={scope}>
+        <TileWrapper>
+          <ImageRow>
+            <ImageBlock imageSrc={background} />
+            <ImageBlock imageSrc={background} />
+            <ImageBlock imageSrc={background} />
+            <ImageBlock imageSrc={background} />
+            <ImageBlock imageSrc={background} />
+            <ImageBlock imageSrc={background} />
+            <ImageBlock imageSrc={background} />
+            <ImageBlock imageSrc={background} />
+          </ImageRow>
+          <ImageRow>
+            <ImageBlock imageSrc={background} />
+            <ImageBlock imageSrc={background} />
+            <ImageBlock imageSrc={background} />
+            <ImageBlock imageSrc={background} />
+            <ImageBlock imageSrc={background} />
+            <ImageBlock imageSrc={background} ref={anchorRef} className={pseudoShadow} />
+            <ImageBlock imageSrc={background} />
+          </ImageRow>
+          <ImageRow>
+            <ImageBlock imageSrc={background} />
+            <ImageBlock imageSrc={background} />
+            <ImageBlock imageSrc={background} />
+            <ImageBlock imageSrc={background} />
+            <ImageBlock imageSrc={background} />
+            <ImageBlock imageSrc={background} />
+            <ImageBlock imageSrc={background} />
+            <ImageBlock imageSrc={background} />
+          </ImageRow>
+        </TileWrapper>
+      </motion.div>
     </div>
   );
 }
