8f46ddc977fb4c3648ed0f1d826307205b94d703
corners2wall <akosto4kin@gmail.com>
2024-02-04 22:04:15 +0300

    fix: delete velik

--- src/components/Animation/AnimatedTextRow.tsx
+++ src/components/Animation/AnimatedTextRow.tsx
@@ -9,37 +9,11 @@ type AnimatedTextRowProps = PropsWithChildren & {
 };
 
 export default function AnimatedTextRow({ children, containerClassName }: AnimatedTextRowProps) {
-  const intersectionOptions: IntersectionOptions = {
-    runningOn: 'bottom',
-    threshold: [0.5, 0.6, 0.7, 0.8, 0.9, 1],
-  };
-
-  const animationOption: AnimationOptions = {
-    end: -15,
-    duration: 500,
-  };
-
-  const ref = useRef<HTMLDivElement>(null);
-
-  const { targetRef, animation } = useScrollAnimation<HTMLDivElement>(
-    intersectionOptions,
-    animationOption
-  );
-
-  const changeTargetPosition = (v: number) => {
-    if (ref.current) ref.current.style.transform = `translateY(${v}px)`;
-  };
-
-  useRequestAnimationFrame(changeTargetPosition, animation);
+  // ToDO
 
   return (
-    <div ref={targetRef}>
-      <div
-        className={`flex items-center mb-[-4.5vw] tracking-[0.05vw] ${containerClassName}`}
-        ref={ref}
-      >
-        {children}
-      </div>
+    <div className={`flex items-center mb-[-4.5vw] tracking-[0.05vw] ${containerClassName}`}>
+      {children}
     </div>
   );
 }
